42a0ecd87b435bf6fa4511591ab4b0d0ed51d2b5
mayragann <mayragann@gmail.com>
2022-07-29 07:42:57 -0400

    Added Intial React Files + Main Game Component

---
+++ react-game/src/App.js
@@ -0,0 +1,12 @@
+import ReactRogue from "./Components/ReactRogue/ReactRogue";
+
+
+function App() {
+  return (
+    <div className="Container">
+      <ReactRogue />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-game/src/Components/ReactRogue/ReactRogue.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./ReactRogue.css"
+
+const ReactRogue = () => {
+  return (
+    <div className='main-section'>ReactRogue</div>
+  )
+}
+
+export default ReactRogue;
